63cb9ef52da1c95ce42312868959f5352633eda6
Rachna Telang <rachnatelang131@gmail.com>
2026-08-13 22:28:24 +0530

    ✅ Java Stdin and Stdout II | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ hackerrank/medium/java-output-formatting/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int i = sc.nextInt();
+         double d = sc.nextDouble();
+         
+         sc.nextLine(); // consume the leftover newline
+         String s = sc.nextLine();
+       
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
