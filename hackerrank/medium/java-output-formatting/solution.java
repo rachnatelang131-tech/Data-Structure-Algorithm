c7f137a84fbe29f8401ac57fa83cc8ec802d5904
Rachna Telang <rachnatelang131@gmail.com>
2026-08-15 23:44:26 +0530

    ✅ Java Output Formatting | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

--- hackerrank/medium/java-output-formatting/solution.java
+++ hackerrank/medium/java-output-formatting/solution.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-         int i = sc.nextInt();
-         double d = sc.nextDouble();
-         
-         sc.nextLine(); // consume the leftover newline
-         String s = sc.nextLine();
-       
-        
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1 = sc.next();
+                int x = sc.nextInt();
+                
+                 System.out.printf("%-15s%03d%n", s1, x);
+                
+            }
+            System.out.println("================================");
+
     }
+    
 }
+
+
+
